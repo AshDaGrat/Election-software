b2dc4c7d1c06a82b74fa13b9cbd8fb4084a0ae30
Ashwin V <ashwin.v336@gmail.com>
2023-01-14 13:52:51 +0530

    check for if usn is in spreadsheet

--- auth.py
+++ auth.py
@@ -1,31 +1,12 @@
 import xlrd
-import xlwt
+#import xlwt
 
-#xlrd boilerplate
-wb = xlrd.open_workbook("usn.xls")
-sheet = wb.sheet_by_index(0)
-n = 3 #number of usn numbers in spreadsheet
-
-#xlwt boilerplate
-
-
-def valid_usn(m):
-    for i in range(n):
-        if int(sheet.cell_value(i,0)) == m:
+def valid_usn(usn, file):
+    wb = xlrd.open_workbook(file)
+    sheet = wb.sheet_by_index(0)
+    for i in range(sheet.ncols):
+        if int(sheet.cell_value(i,0)) == usn:
             return True
-            break
     return False
-    
-"""
-def freeindex():
-    for i in range(10000):
-        if 
 
-def addtosheet():
-
-"""
-
-
-print(valid_usn(1346))
-
-
+print(valid_usn(12345, "Election-software\\usn.xls"))
