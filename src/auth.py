51b9a4e98601b208db0cb952a4ba0b2cfccc2a13
Ashwin V <77207196+AshDaGrat@users.noreply.github.com>
2023-01-17 12:11:30 +0530

    Add files via upload

--- src/auth.py
+++ src/auth.py
@@ -1,5 +1,6 @@
 import xlrd
-import xlwt
+import openpyxl
+
 
 def valid_usn(usn, file):
 
@@ -8,22 +9,41 @@ def valid_usn(usn, file):
     NEEDS TO BE .XLS FILE TYPE. CANNOT BE .XLSX TYPE
     Returns True if USN is in the first column of the first sheet of the file
 
-    USECASE : 
-    valid_usn(12345, "Election-software\\data\\usn.xls") 
+    USECAGE : 
+    valid_usn(12345, "data\\usn.xls") 
     >>> True
     """
 
     wb = xlrd.open_workbook(file)
     sheet = wb.sheet_by_index(0)
+    
     for i in range(sheet.ncols):
         if int(sheet.cell_value(i,0)) == int(usn):
             return True
     return False
 
-def add_to_sheet(usn, file):
+
+def add_to_excel(num, file_path):
+
     """
-    TODO
-    1. open sheet as per path
-    2. find the first free index in the first column
-    3. add usn as int type to that index
+    Takes two parameters, USN number, and the path to the .xlsx file
+    NEEDS TO BE .XLSX FILE TYPE. CANNOT BE .XLS TYPE
+    adds usn to first empty row in first column of sheet
+
+    USEAGE : 
+    add_to_excel(1234, "data\\done.xlsx")
     """
+
+    wb = openpyxl.load_workbook(file_path)
+    sheet = wb.active
+
+    # Find the first empty row in the sheet
+    a = 0
+    for row in sheet.iter_rows():
+        for cell in row:
+            a+=1
+
+    cell = sheet.cell(row = a, column=1)
+    cell.value = num
+    # Save the changes to the workbook
+    wb.save(file_path)
