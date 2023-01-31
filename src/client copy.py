3f4062a0fbeb1170fac230c302d49854ea7059dd
ayaanjamil <ayaanjamil@gmail.com>
2023-01-31 11:26:31 +0530

    Auto stash before merge of "main" and "origin/main"

---
+++ src/client copy.py
@@ -0,0 +1,24 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.50.98"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+send(str([1, 2, 3, 4]))
