4434c616915ee5357b521cd5beadcd466d9d47c5
Ashwin V <77207196+AshDaGrat@users.noreply.github.com>
2023-02-01 00:25:30 +0530

    login page front end test

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 import eel
-from random import randint
 import auth
 
-eel.init("Election-software\\src\\web")
+eel.init("src\\web")
 
 # login
 @eel.expose	
 def login(input_val):
 	print(input_val)
-	auth.add_to_excel(input_val, "Election-software\\data\\done.xlsx")
-	return auth.valid_usn(input_val, "Election-software\\data\\usn.xlsx")
+	auth.add_to_excel(input_val, "data\\done.xlsx")
+	return auth.valid_usn(input_val, "data\\usn.xlsx")
 
 # Start the index.html file
 eel.start("index.html")
