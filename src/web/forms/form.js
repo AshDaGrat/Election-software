16a2205782ba8ea613b5ea42cdf34c03129914ba
Ashwin V <ashwin.v336@gmail.com>
2023-02-02 14:50:59 +0530

    js forms test

--- src/web/forms/form.js
+++ src/web/forms/form.js
@@ -8,3 +8,32 @@ function getSelectedOption() {
     }
     eel.test(value)
 }
+
+const bold = document.querySelector('.bold')
+const italic = document.querySelector('.italic')
+const editable = document.querySelector('.editable')
+
+editable.innerHTML = `
+<div class="leftside">
+<label class="labelexpanded">
+    <input type="radio" id="Ayaan Jamil" name="options" value="Ayaan Jamil">
+    <div class="radio-btns">
+        <img src="../assets/yea.png">
+        <p>Ayaan Jamil</p>
+    </div>
+</input>
+</label>
+<label class="labelexpanded">
+    <input type="radio" id="Jyaan Aamil" name="options" value="Jyaan Aamil">
+    <div class="radio-btns">
+        <img src="../assets/yea.png">
+        <p>Jyaan Aamil</p>
+    </div>
+</input>
+</label>
+
+
+<button type="button" class='radio-btns' onclick="getSelectedOption();">button</button>
+</div>
+`;
+
