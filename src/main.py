7f6d5b537cde30881f61c301f1606ba2a8c1361b
Ashwin V <ashwin.v336@gmail.com>
2023-02-02 09:08:39 +0530

    working usn checker

--- src/main.py
+++ src/main.py
@@ -9,10 +9,10 @@ eel.init("web")
 def login(input_val):
 	input_val = int(input_val)
 	print(input_val)
-	if auth.valid_usn(input_val, "data/done.xlsx"): return 1 #check for if they have already voted
-	if not(auth.valid_usn(input_val, "data/usn.xlsx")): return 2 #check for invalid USN
+	if auth.valid_usn(input_val, "../data/done.xlsx"): return 1 #check for if they have already voted
+	if not(auth.valid_usn(input_val, "../data/usn.xlsx")): return 2 #check for invalid USN
 	else:
-		auth.add_to_excel(input_val, "data/done.xlsx")
+		auth.add_to_excel(input_val, "../data/done.xlsx")
 		return 3
 
 
