87487dec63a6afc243fc9f172fdeaa2d2a7bbcd2
Ashwin V <ashwin.v336@gmail.com>
2023-02-02 07:44:29 +0530

    autoconnect to server directly from main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import eel
 import auth
+import socket
 
 eel.init("src\\web")
 
@@ -19,5 +20,46 @@ def login(input_val):
 def test(a):
 	print(a)
 
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.50.98"
+ADDR = (SERVER, PORT)
+
+CHOICES = {'usn': 100212,
+           'spl-b': 'raju',
+           'spl-g': 'kaju',
+           'cs-b': 'gaju',
+           'cs-g': 'ayo',
+           'ss-b': 'kalia',
+           'ss-g': 'dholu',
+           'aspl-b': 'bholu',
+           'aspl-g': 'priti bala',
+           'acs-b': 'varnika',
+           'acs-g': 'saswath',
+           'ass-b': 'bill cosby',
+           'ass-g': 'bill sinby',
+           'ic-p' : 'the abhuraj',
+           'ic-vp': 'nigin',
+           'ic-sec': 'therila',
+           'ic-t': 'pankti'
+           }
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+    
 # Start the index.html file
 eel.start("index.html")
+
+#send(str(CHOICES))
